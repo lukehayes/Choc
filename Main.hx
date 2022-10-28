ce7cde21d03e6ba35695452d1d841198c25f3502
Luke Hayes <lukepurplehayes@gmail.com>
2022-10-28 10:26:38 +0100

    Cleared main to make way for Choc.

--- Main.hx
+++ Main.hx
@@ -1,22 +1,29 @@
+import Choc.World;
+
+
 class Main extends hxd.App {
 
     var o : h2d.Object;
     var g : h2d.Graphics;
+    var c : Float = 0.0;
+    var world : World;
 
     override function init() {
         g = new h2d.Graphics(s2d);
         o = new h2d.Object();
+        world = World.new();
 
-        g.beginFill(0xEA8220);
-        g.drawRect(10, 10, 30, 200);
-        g.endFill();
     }
 
     override function update(dt:Float) 
     {
-        g.x = Math.cos(c) * this.speed * dt;
-        g.y = Math.sin(c) * this.speed * dt;
+        g.x = Math.cos(c) * 10 * dt;
+        g.y = Math.sin(c) * 10 * dt;
         c++;
+
+        g.beginFill(0xEA8220);
+        g.drawRect(10, 10, 30, 200);
+        g.endFill();
     }
 
     static function main() {
