5ddca21851336f884b32a82fc60d4a6113dc68a7
Luke Hayes <lukepurplehayes@gmail.com>
2022-10-28 10:26:07 +0100

    Added System class to project.

---
+++ Choc/System/System.hx
@@ -0,0 +1,19 @@
+package Choc.System;
+
+/**
+  Base class for all systems.
+**/
+class System
+{
+    var name : String;
+
+    public function new(name)
+    {
+        this.name = name;
+    }
+
+    public function update(dt: Float)
+    {
+        trace("Updating " + this.name);
+    }
+}
