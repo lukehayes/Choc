be76bc12670fab978da3284dd954b42aeb831887
Luke Hayes <lukepurplehayes@gmail.com>
2022-10-29 11:43:20 +0100

    Added choc.component.Component class to project.

---
+++ choc/component/Component.hx
@@ -0,0 +1,24 @@
+package choc.component;
+
+/**
+  Base class for all components.
+**/
+class Component
+{
+    public var name : String;
+
+    public function new(name)
+    {
+        this.name = name;
+    }
+
+    public function toString()
+    {
+        return this.name + " Component";
+    }
+
+    public function update(dt: Float)
+    {
+        trace("Updating " + this);
+    }
+}
