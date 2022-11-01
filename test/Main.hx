ca244000a57eb3d983c5a19185dc09ab10d69f77
Luke Hayes <lukepurplehayes@gmail.com>
2022-11-01 16:41:07 +0000

    Added instance of DrawSystem to Main.hx

--- test/Main.hx
+++ test/Main.hx
@@ -1,6 +1,6 @@
 package test;
 import choc.World;
-import choc.system.System;
+import choc.system.DrawSystem;
 
 
 class Main extends hxd.App {
@@ -9,28 +9,28 @@ class Main extends hxd.App {
     var g : h2d.Graphics;
     var c : Float = 0.0;
     var world : World;
-    var system : System;
+    var system : DrawSystem;
 
     override function init() {
         g = new h2d.Graphics(s2d);
         o = new h2d.Object();
         world = new World();
-        system  = new System("Position");
-        world.systems[0] = system;
+        system  = new DrawSystem("Draw");
 
+        world.addSystem(0, system);
     }
 
     override function update(dt:Float) 
     {
-        trace(system);
-        g.x = Math.cos(c) * 10 * dt;
-        g.y = Math.sin(c) * 10 * dt;
-        c++;
+        //trace(system);
+        //g.x = Math.cos(c) * 10 * dt;
+        //g.y = Math.sin(c) * 10 * dt;
+        //c += 0.1;
 
-        g.beginFill(0x00ff00);
-        g.drawRect(15, 10, 30, 200);
-        g.endFill();
-        system.update(dt);
+        //g.beginFill(0x00ff00);
+        //g.drawRect(100, 110, 330, 200);
+        //g.endFill();
+        //system.update(dt);
     }
 
     static function main() {
