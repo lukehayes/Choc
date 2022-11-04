3764584b8bf3fbc6a20101c18f7efb35c756dea1
Luke Hayes <lukepurplehayes@gmail.com>
2022-11-04 09:32:57 +0000

    Added component retrieval helper methods to Entity.

--- choc/entity/Entity.hx
+++ choc/entity/Entity.hx
@@ -13,8 +13,41 @@ class Entity
         this.components = new Map<String, Component>();
     }
 
+    /**
+      Add a new component to the entity.
+
+      @param name         The name of the component.
+
+      @param component    An instance of Component.
+
+      @return Bool
+    **/
     public function addComponent(name: String, component: Component)
     {
         this.components[name] = component;
+    }
+
+    /**
+      Check a particular component exists for this entity.
+
+      @param name    The name of the component.
+
+      @return Bool
+    **/
+    public function hasComponent(name: String) : Bool
+    {
+        return !!this.components.exists(name);
+    }
+
+    /**
+      Get a particular component from this entities Map.
+
+      @param name    The name of the component.
+
+      @return The Component class if found, null otherwise.
+    **/
+    public function getComponent(name: String) : Component
+    {
+        return this.components[name];
     }
 }
