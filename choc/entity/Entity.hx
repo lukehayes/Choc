62b6397b1a42d400fbe45bb4f932f0ab31c9e6ed
Luke Hayes <lukepurplehayes@gmail.com>
2022-11-04 09:04:16 +0000

    Added missing Map<String, Component> instantiation in Entity base class.

--- choc/entity/Entity.hx
+++ choc/entity/Entity.hx
@@ -1,6 +1,7 @@
 package choc.entity;
 
 import choc.component.Component;
+
 /**
   Base class for all entities.
 **/
@@ -8,7 +9,9 @@ class Entity
 {
     public var components : Map<String, Component>;
 
-    public function new(){}
+    public function new(){
+        this.components = new Map<String, Component>();
+    }
 
     public function addComponent(name: String, component: Component)
     {
