e806d74c6f19056cbd518eab4217a45d3ceeb360
Luke Hayes <lukepurplehayes@gmail.com>
2022-11-08 10:43:44 +0000

    Added ColorComponent.random() helper method.

--- choc/component/std/ColorComponent.hx
+++ choc/component/std/ColorComponent.hx
@@ -1,6 +1,7 @@
 package choc.component.std;
 
 import choc.component.Component;
+import Random;
 
 /**
   Color component contains RGB hex value,
@@ -16,5 +17,21 @@ class ColorComponent extends Component
     {
         super("Color");
         this.color = color;
+    }
+
+    /**
+      Generate a random color value.
+     **/
+    public function random() : Int
+    {
+        var values = ["0","1","2","3","4","5","6","7","8","9","a","b","c","d","e","f"];
+        var color = "0x";
+
+        for(i in 0...6)
+        {
+            color += Random.fromArray(values);
+        }
+
+        return Std.parseInt(color);
     }
 }
