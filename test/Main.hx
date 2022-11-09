ad8a4b2ae5372a2970cf82f1b8d713e0d6338825
Luke Hayes <lukepurplehayes@gmail.com>
2022-11-09 10:08:14 +0000

    Removed properties that are no longer needed in Main.

--- test/Main.hx
+++ test/Main.hx
@@ -10,23 +10,19 @@ import Random;
 
 class Main extends hxd.App {
 
-    var o : h2d.Object;
     var g : h2d.Graphics;
-    var c : Float = 0.0;
     var world : World;
     var drawSystem : DrawSystem;
     var entity : TestEntity;
 
     override function init() {
         g = new h2d.Graphics(s2d);
-        o = new h2d.Object();
         world = World.instance;
 
         drawSystem  = new DrawSystem("Draw", g);
         world.addSystem(0, drawSystem);
 
         EntityFactory.generate(100);
-
     }
 
     override function update(dt:Float) 
