8963510857ed6367d30e889ddae8023c034e077d
Luke Hayes <lukepurplehayes@gmail.com>
2022-11-10 09:04:46 +0000

    Test entities width and height are now generated randomly.

--- test/EntityFactory.hx
+++ test/EntityFactory.hx
@@ -24,13 +24,15 @@ class EntityFactory
         {
             var rx  = Random.int(0,400);
             var ry  = Random.int(0,400);
+            var rw  = Random.int(5,40);
+            var rh  = Random.int(5,40);
             var rdx = Random.int(-1,1);
             var rdy = Random.int(-1,1);
 
             var e = new TestEntity();
 
             e.addComponent("Transform",
-                new TransformComponent(rx, ry, rdx, rdy, 10,10, Random.int(150,400))
+                new TransformComponent(rx, ry, rdx, rdy, rw,rh, Random.int(150,400))
             );
 
             var c = new ColorComponent(0xAAAAAA);
