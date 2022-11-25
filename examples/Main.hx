057f18ab5eada5b955f5fa9b7d1a5bd2de639909
Luke Hayes <lukepurplehayes@gmail.com>
2022-11-25 09:49:14 +0000

    Created simple example Main.hx.

---
+++ examples/Main.hx
@@ -0,0 +1,39 @@
+package examples;
+
+import choc.World;
+import choc.system.DrawSystem;
+import choc.system.MovementSystem;
+import choc.component.std.TransformComponent;
+import choc.component.std.ColorComponent;
+import test.TestEntity;
+import test.EntityFactory;
+import Random;
+
+class Main extends hxd.App {
+
+    var g : h2d.Graphics;
+    var world : World;
+
+    override function init() {
+        g = new h2d.Graphics(s2d);
+        world = World.instance;
+
+        var drawSystem  = new DrawSystem(g);
+        var moveSystem = new MovementSystem();
+
+        world.addSystem(drawSystem);
+        world.addSystem(moveSystem);
+
+        EntityFactory.generate(10);
+    }
+
+    override function update(dt:Float) 
+    {
+        world.update(dt);
+    }
+
+    static function main() {
+        new Main();
+    }
+
+}
