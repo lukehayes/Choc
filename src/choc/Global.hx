82dfaad6fe3bc96637c6645746189ed701579c00
lukehayes <lukepurplehayes@gmail.com>
2023-07-20 16:55:31 +0100

    Added choc.Global class to project.

---
+++ src/choc/Global.hx
@@ -0,0 +1,17 @@
+package choc;
+
+/**
+ * Useful configuration constants are stored in here.
+ */
+final class Global
+{
+    /** ECS CONSTANTS ------------------------------------------------------ */
+    public static final
+        TRANSFORM_COMPONENT_INDEX = 0;
+
+    public static final
+        ENTITY_COUNT              = 10;
+
+    public static final
+        COMPONENT_COUNT           = 4;
+}
