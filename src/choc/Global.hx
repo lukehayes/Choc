3c770f11ef76c1105780f5fadbd28d0a0c9f18c2
Luke Hayes <lukepurplehayes@gmail.com>
2023-07-21 09:44:19 +0100

    Added choc.Global class

---
+++ src/choc/Global.hx
@@ -0,0 +1,14 @@
+package choc;
+
+
+/**
+  Useful helper constnats are defined in this file.
+ */
+class Global
+{
+    public static final
+        TRANSFORM_COMPONENT_INDEX = 0;
+
+    public static final
+        ENTITY_COUNT = 10;
+}
