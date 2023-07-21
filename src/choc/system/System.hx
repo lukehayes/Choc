15d59fef8c05f35c63699ce30c775fe07d07f019
Luke Hayes <lukepurplehayes@gmail.com>
2023-07-21 10:16:15 +0100

    Removed pointless implementation from choc.system.System.

--- src/choc/system/System.hx
+++ src/choc/system/System.hx
@@ -1,8 +1,5 @@
 package choc.system;
 
-import choc.component.Component;
-import choc.entity.Entity;
-
 /**
   Base class for all systems. 
 
@@ -10,20 +7,8 @@ import choc.entity.Entity;
 **/
 abstract class System
 {
-    /**
-      The name of the system.
-    **/
+    /** The name of the system. **/
     public var name : String;
-
-    /**
-      List of components the system requires.
-    **/
-    public var requirements: Array<String>;
-
-    /**
-      List of all entities that have the required components.
-    **/
-    private var filteredEntities : Array<Entity>;
 
     /**
       Constructor.
@@ -33,53 +18,6 @@ abstract class System
     public function new(name)
     {
         this.name = name;
-        this.requirements   = [];
-        this.filteredEntities = [];
-    }
-
-    /**
-      List all of the components this system needs to work.
-
-      @param args    All of the required components.
-
-      @return Array
-    **/
-    public function requires(...args:String) : Array<String>
-    {
-        for(arg in args)
-        {
-            this.requirements.push(arg);
-        }
-
-        return this.requirements;
-    }
-
-    /**
-      Loop through all of the entities in the world and extract all of
-      the ones have have the required components the system needs.
-
-      Runs once every frame.
-    **/
-    public function filterEntities()
-    {
-        for(entity in World.instance.entities)
-        {
-            var meetsRequirement = false;
-            var requirementCount = 0;
-
-            for(req in this.requirements)
-            {
-                if(entity.hasComponent(req))
-                {
-                    requirementCount += 1;
-                }
-            }
-
-            if(requirementCount == requirements.length)
-            {
-                this.filteredEntities.push(entity);
-            }
-        }
     }
 
     /**
