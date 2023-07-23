3bcca23496993d65290a40141da5f1c078eb1780
Luke Hayes <lukepurplehayes@gmail.com>
2023-07-23 10:30:12 +0100

    Stared DrawSystem rewrite.

--- src/choc/system/DrawSystem.hx
+++ src/choc/system/DrawSystem.hx
@@ -14,14 +14,30 @@ class DrawSystem extends System
 {
     private var g : h2d.Graphics;
 
+    private var entities = null;
+
     public function new(ents : Entities)
     {
         super("Draw");
         this.g = new h2d.Graphics();
+
+        this.entities = ents;
     }
 
     public function update(dt: Float)
     {
+
         this.g.clear();
+        trace(this.entities.length);
+        for(e in this.entities)
+        {
+            trace(e);
+            var transform_comp = cast(e[Global.TRANSFORM_COMPONENT_INDEX], TransformComponent);
+
+            this.g.beginFill(0xFF00FF);
+            this.g.drawRect(10,10,10,10);
+            this.g.endFill();
+        }
+
     }
 }
