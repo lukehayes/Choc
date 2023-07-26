aea8acaa7f202239ea90c67fdef2d6ed7e95348c
Luke Hayes <lukepurplehayes@gmail.com>
2023-07-26 09:15:59 +0100

    Added simple screen bounds check to MoveSystem.

--- src/choc/system/MoveSystem.hx
+++ src/choc/system/MoveSystem.hx
@@ -29,6 +29,19 @@ class MoveSystem extends System
             {
                 tc.x += tc.dx + tc.speed * dt;
                 tc.y += tc.dy + tc.speed * dt;
+
+                // TODO Create a proper collision system solution.
+                //
+                // Edge of screen check.
+                if(tc.x < 2 || tc.x > 580)
+                {
+                    tc.dx = -tc.dx;
+                }
+
+                if(tc.y < 2 || tc.y > 580)
+                {
+                    tc.dy = -tc.dy;
+                }
             }
 
         }
