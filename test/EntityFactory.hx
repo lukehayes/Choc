9981cb3d78d2f5b552c5d232b50fe708350adc9e
Luke Hayes <lukepurplehayes@gmail.com>
2023-07-30 10:11:21 +0100

    Removed EntityFactory.generateEntities().

--- test/EntityFactory.hx
+++ test/EntityFactory.hx
@@ -19,47 +19,11 @@ class EntityFactory
 
       @param count    The number of entities to add to the world.
 
-      @return Array<Array<Component>>;
-     **/
-    static public function generateEntities() : Entities
-    {
-        var entities = [];
-
-        for(i in 0...Global.ENTITY_COUNT)
-        {
-            var components = new Array<Component>();
-
-            var rx  = Random.int(0,400);
-            var ry  = Random.int(0,400);
-            var rsz = Random.int(2,10);
-            var rsp = Random.int(2,130);
-            var rdx = Random.int(-1,1);
-            var rdy = Random.int(-1,1);
-
-            var chance = Random.int(0,10);
-
-            components[Global.TRANSFORM_COMPONENT_INDEX] = new TransformComponent(rx, ry, rsz, rsz, rsp, rdx, rdy);
-            components[Global.SPRITE_COMPONENT_INDEX] = chance % 2 == 0 ? new SpriteComponent('debug.png',20,20) : null;
-
-            entities[i] = components;
-        }
-
-        return entities;
-    }
-
-
-    /**
-      Generate N amount of entities for testing.
-
-      @param count    The number of entities to add to the world.
-
       @return Void
      **/
-    static public function generate(count:Int) : Void
+    static public function generate(count:Int, world : World) : Void
     {
-        var world  = World.instance;
-
-        for(i in 0...Global.ENTITY_COUNT - 1)
+        for(i in 0...count - 1)
         {
             var rx  = Random.int(0,400);
             var ry  = Random.int(0,400);
@@ -74,7 +38,8 @@ class EntityFactory
                 new TransformComponent(rx, ry, rdx, rdy, rw,rh, Random.int(150,400))
             );
 
-            world.addEntity(i,e);
+
+            world.addEntity(e);
         }
 
     }
